7b21d89684d96aa4d253fe285e84b1e980ac6a98
shmoon <81969210+moonstrnck@users.noreply.github.com>
2024-09-10 16:33:43 +0900

    Setting: 프로젝트 초기 세팅
    - eslint-lint-airbnb, prettier 설정
    - husky, lint-staged
    - sass
    - json-server

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import './globals.scss';
+
+const pretendard = localFont({
+  src: './fonts/PretendardVariable.woff2',
+  variable: '--font-pretendard',
+  weight: '100 900',
+});
+
+export const metadata: Metadata = {
+  title: 'Todo List',
+  description: 'Todo List',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${pretendard.variable}`}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.scss';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>Todo List</h1>
+    </div>
+  );
+}
