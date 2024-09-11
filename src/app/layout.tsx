6e9f924a480bd39bf3f02abb53a5da67b861cd97
shmoon <81969210+moonstrnck@users.noreply.github.com>
2024-09-12 02:01:34 +0900

    Style: globals.scss 경로 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
-import './globals.scss';
+import './styles/globals.scss';
 
 const pretendard = localFont({
   src: './fonts/PretendardVariable.woff2',
