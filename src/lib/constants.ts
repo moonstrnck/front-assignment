29ba41d658e21290721687d3aeb081ddc0b30b29
shmoon <81969210+moonstrnck@users.noreply.github.com>
2024-09-12 10:08:25 +0900

    Add: 상수 파일 추가

---
+++ src/lib/constants.ts
@@ -0,0 +1,12 @@
+export const TODOS_API = 'http://localhost:3001/todos';
+
+export const ERROR_MESSAGES = {
+  ERROR_TITLE: '❗️ 오류',
+  UPDATE_TODO: '할 일 업데이트에 실패했습니다. 다시 시도해 주세요.',
+  DELETE_TODO: '할 일 삭제에 실패했습니다. 다시 시도해 주세요.',
+  GENERIC_ERROR: '오류가 발생했습니다. 다시 시도해 주세요.',
+};
+
+export const DIALOG_MESSAGES = {
+  CONFIRM_DELETE: '할 일을 정말 삭제할까요?',
+};
