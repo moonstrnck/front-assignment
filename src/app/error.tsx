6658cf46a510eb990d92385c2642455c13deee05
shmoon <81969210+moonstrnck@users.noreply.github.com>
2024-09-12 18:34:32 +0900

    Design: Error 컴포넌트 스타일 수정

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect } from 'react';
+import Image from 'next/image';
 import Button from '@/app/components/common/Button';
 import styles from '@/app/error.module.scss';
 
@@ -17,8 +18,14 @@ export default function Error({
 
   return (
     <div className={styles.errorContainer}>
+      <Image
+        src="/images/icon-error.png"
+        alt="오류 발생"
+        width={120}
+        height={120}
+      />
       <h2 className={styles.errorTitle}>오류가 발생했습니다.</h2>
-      <Button theme="dangerous" onClick={() => reset()}>
+      <Button theme="primary" onClick={() => reset()}>
         다시 시도
       </Button>
     </div>
