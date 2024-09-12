233db12e5a45669bb5fe0a89ac5aed5fb1fc5fe8
shmoon <81969210+moonstrnck@users.noreply.github.com>
2024-09-12 20:16:21 +0900

    Refactor: 오류 메시지 수정

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,7 @@ export const TODOS_API = 'http://localhost:3001/todos';
 
 export const ERROR_MESSAGES = {
   ERROR_TITLE: '❗️ 오류',
-  UPDATE_TODO: '할 일 업데이트에 실패했습니다. 다시 시도해 주세요.',
+  UPDATE_TODO: '할 일 수정에 실패했습니다. 다시 시도해 주세요.',
   DELETE_TODO: '할 일 삭제에 실패했습니다. 다시 시도해 주세요.',
   CREATE_TODO: '할 일 생성에 실패했습니다. 다시 시도해 주세요.',
   GENERIC_ERROR: '오류가 발생했습니다. 다시 시도해 주세요.',
