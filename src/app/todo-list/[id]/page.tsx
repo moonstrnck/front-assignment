012c4f3fc338b21113a477f74839fd1389b15689
shmoon <81969210+moonstrnck@users.noreply.github.com>
2024-09-12 10:35:43 +0900

    Feat: TodoDetailPage 서버 사이드 페칭
    - 선택한 Todo의 초기 데이터 페칭

--- src/app/todo-list/[id]/page.tsx
+++ src/app/todo-list/[id]/page.tsx
@@ -1,29 +1,7 @@
 import TodoDetail from '@/app/components/todo/TodoDetail';
 import styles from '@/app/todo-list/[id]/page.module.scss';
 
-async function getTodo(id: string) {
-  // TODO: json-server에서 todo를 가져오는 로직 구현
-  return {
-    id,
-    title: '샘플 할 일',
-    content: '이것은 샘플 할 일 내용입니다.',
-    completed: false,
-  };
-}
-
-async function updateTodo(id: string, title: string, content: string) {
-  'use server';
-
-  // TODO: json-server에 todo를 업데이트하는 로직 구현
-  return { id, title, content, completed: false };
-}
-
-async function deleteTodo(id: string) {
-  'use server';
-
-  // TODO json-server에서todo를 삭제하는 로직 구현
-  console.log('Deleting todo:', id);
-}
+import { getTodo } from '@/lib/api';
 
 export default async function TodoDetailPage({
   params,
@@ -34,7 +12,7 @@ export default async function TodoDetailPage({
 
   return (
     <div className={styles.container}>
-      <TodoDetail todo={todo} updateTodo={updateTodo} deleteTodo={deleteTodo} />
+      <TodoDetail todo={todo} />
     </div>
   );
 }
